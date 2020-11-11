9d9679c2c914ed46472acc81460d6fc5343c943f
Виктор Елсуков <beerbis@yandex.ru>
2020-11-12 02:32:20 +0300

    feat: реализовать Задание к уроку 6

---
+++ task1/src/Task6/Cat.java
@@ -0,0 +1,22 @@
+package Task6;
+
+public class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    protected double getBasicRunLimit() {
+        return 200;
+    }
+
+    @Override
+    protected double getBasicJumpLimit() {
+        return 2;
+    }
+
+    @Override
+    protected double getBasicSwimLimit() {
+        return 0;
+    }
+}
---
+++ task1/src/Task6/Dog.java
@@ -0,0 +1,22 @@
+package Task6;
+
+public class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    protected double getBasicRunLimit() {
+        return 500;
+    }
+
+    @Override
+    protected double getBasicJumpLimit() {
+        return 0.5;
+    }
+
+    @Override
+    protected double getBasicSwimLimit() {
+        return 10;
+    }
+}
