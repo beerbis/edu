613cbe094784af4ebb7f9bc694a2d66dec56ed9f
Виктор Елсуков <beerbis@yandex.ru>
2020-11-16 21:06:10 +0300

    feat: реализовать виндоподобный gui-калькулятор

---
+++ task1/src/Calc/CalcOperation.java
@@ -0,0 +1,59 @@
+package Calc;
+
+public abstract class CalcOperation {
+    private char id;
+    private String name;
+    abstract public double perform(double a, double b);
+
+    public CalcOperation(char id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public char getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static CalcOperation plus = new CalcOperation('+', "plus") {
+        @Override
+        public double perform(double a, double b) {
+            return a + b;
+        }
+    };
+
+    public static CalcOperation minus = new CalcOperation('-', "minus") {
+        @Override
+        public double perform(double a, double b) {
+            return a - b;
+        }
+    };
+
+    public static CalcOperation div = new CalcOperation('/', "divide") {
+        @Override
+        public double perform(double a, double b) {
+            return a / b;
+        }
+    };
+
+    public static CalcOperation mul = new CalcOperation('*', "multiply") {
+        @Override
+        public double perform(double a, double b) {
+            return a * b;
+        }
+    };
+
+    public static CalcOperation getOperationById(char Id) {
+        switch (Id) {
+            case '+': return plus;
+            case '-': return minus;
+            case '*': return mul;
+            case '/': return div;
+        }
+
+        return null;
+    }
+}
