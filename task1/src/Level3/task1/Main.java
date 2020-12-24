29bfbfc08f177fc5be550e764c50059033583029
Виктор Елсуков <beerbis@yandex.ru>
2020-12-25 01:47:49 +0300

    feat: реализовать тестовый пример реботы с коробками

--- task1/src/Level3/task1/Main.java
+++ task1/src/Level3/task1/Main.java
@@ -14,8 +14,9 @@ public class Main {
         List<Integer> list = toArrayList(ints);
         System.out.println(list);
 
-
+        playWithBoxes();
     }
+
 
     private static <T> void swap(T[] elems, int i1, int i2) {
         T tmp = elems[i2];
@@ -27,5 +28,38 @@ public class Main {
         return new ArrayList<T>(Arrays.asList(elems));
     }
 
+    static private final class AppleBox extends FruitBox<Apple>{
+
+        public AppleBox(Apple... fruits) { super(fruits); }
+
+        @Override
+        protected float getSingleFruitWeight() { return 1f; }
+    }
+
+    static private final class OrangeBox extends FruitBox<Orange>{
+
+        public OrangeBox(Orange... fruits) { super(fruits); }
+
+        @Override
+        protected float getSingleFruitWeight() { return 1.5f; }
+    }
+
+    private static void playWithBoxes() {
+        final String eq = "Apples weight %s, oranges - %s. Коробки равны по весу";
+        final String noteq = "Apples weight %s, oranges - %s. Коробки не равны по весу";
+
+        AppleBox appleBox = new AppleBox(new Apple(), new Apple());
+        OrangeBox orangeBox = new OrangeBox(new Orange(), new Orange());
+        System.out.println(String.format(appleBox.compare(orangeBox) ? eq : noteq, appleBox.getWeight(), orangeBox.getWeight()));
+
+        appleBox.add(new Apple());
+        System.out.println(String.format(appleBox.compare(orangeBox) ? eq : noteq, appleBox.getWeight(), orangeBox.getWeight()));
+
+        AppleBox appleBox1 = new AppleBox();
+        appleBox.pourOver(appleBox1);
+
+        System.out.println("old apple box got " + appleBox.getWeight());
+        System.out.println("new apple box became " + appleBox1.getWeight());
+    }
 
 }
