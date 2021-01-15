4e056c7d32f3cbd127eec9c450f5ff7ac37fe456
Виктор Елсуков <beerbis@yandex.ru>
2021-01-15 03:04:00 +0300

    refactor: заменить класс PrintingInstance на использование замыканий
    Примечания
    =========
    Минусы, теперь мыполучили вместо одного класса - три,
    Плюсы, кода меньше. PrintingInstance был простым хранилищем, ровно чем занимаются анонимные замыкания.

--- task1/src/Level3/waitingQueue/WaitingPrinter.java
+++ task1/src/Level3/waitingQueue/WaitingPrinter.java
@@ -1,8 +1,5 @@
 package Level3.waitingQueue;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class WaitingPrinter {
 
     private static final int COUNT = 5;
@@ -12,9 +9,17 @@ public class WaitingPrinter {
 
     public static void main(String[] args) {
         WaitingPrinter printer = new WaitingPrinter();
-        new Thread(new PrintingInstance(printer,'A', 'C', true)).start();
-        new Thread(new PrintingInstance(printer,'B', 'A')).start();
-        new Thread(new PrintingInstance(printer,'C', 'B')).start();
+        new Thread(() -> {
+            for (int i = 1; i <= COUNT; i++) printer.printOne('A', 'C', true);
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 1; i <= COUNT; i++) printer.printOne('B', 'A', false);
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 1; i <= COUNT; i++) printer.printOne('C', 'B', false);
+        }).start();
     }
 
     private synchronized void printOne(char literal, char trigger, boolean triggerOnNoChar) {
@@ -31,28 +36,5 @@ public class WaitingPrinter {
         System.out.print(literal);
         lastPrinted = literal;
         notifyAll();
-    }
-
-    static private class PrintingInstance implements Runnable {
-        private WaitingPrinter owner;
-        private final char literal;
-        private final char trigger;
-        private final boolean triggerOnNoChar;
-
-        public PrintingInstance(WaitingPrinter owner, char literal, char trigger) {
-            this(owner, literal, trigger, false);
-        }
-
-        public PrintingInstance(WaitingPrinter owner, char literal, char trigger, boolean triggerOnNoChar) {
-            this.owner = owner;
-            this.literal = literal;
-            this.trigger = trigger;
-            this.triggerOnNoChar = triggerOnNoChar;
-        }
-
-        @Override
-        public void run() {
-            for (int i = 1; i <= COUNT; i++) owner.printOne(literal, trigger, triggerOnNoChar);
-        }
     }
 }
