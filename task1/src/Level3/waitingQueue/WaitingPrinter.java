c6bc202f679cacd2ce9a1a3423ed1721c0dbaea5
Виктор Елсуков <beerbis@yandex.ru>
2021-01-15 03:39:50 +0300

    refactor: не выходить специально из монитора каждый раз после печати
    Мотивация
    ========
    Интуиция говорит, что цикл надо крутить над монитором, но как факт, можно его внести под монитор.
    Выполняем notify, а после, либо выходим из метода - мониотр освободится, либо дойдём до wait - монитор опять же освободится.

--- task1/src/Level3/waitingQueue/WaitingPrinter.java
+++ task1/src/Level3/waitingQueue/WaitingPrinter.java
@@ -10,28 +10,26 @@ public class WaitingPrinter {
     public static void main(String[] args) {
         WaitingPrinter printer = new WaitingPrinter();
 
-        new Thread(() -> printer.printThemAll('A', 'C', true)).start();
-        new Thread(() -> printer.printThemAll('B', 'A', false)).start();
-        new Thread(() -> printer.printThemAll('C', 'B', false)).start();
+        new Thread(() -> printer.print('A', 'C', true)).start();
+        new Thread(() -> printer.print('B', 'A', false)).start();
+        new Thread(() -> printer.print('C', 'B', false)).start();
     }
 
-    private void printThemAll(char literal, char trigger, boolean triggerOnNoChar) {
-        for (int i = 1; i <= COUNT; i++) printOne(literal, trigger, triggerOnNoChar);
-    }
-
-    private synchronized void printOne(char literal, char trigger, boolean triggerOnNoChar) {
-        while (lastPrinted != trigger && (!triggerOnNoChar || lastPrinted != NO_CHAR)) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                Thread.currentThread().interrupt();
-                return;
+    private synchronized void print(char literal, char trigger, boolean triggerOnNoChar) {
+        for (int i = 1; i <= COUNT; i++) {
+            while (lastPrinted != trigger && (!triggerOnNoChar || lastPrinted != NO_CHAR)) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                    return;
+                }
             }
-        }
 
-        System.out.print(literal);
-        lastPrinted = literal;
-        notifyAll();
+            System.out.print(literal);
+            lastPrinted = literal;
+            notifyAll();
+        }
     }
 }
