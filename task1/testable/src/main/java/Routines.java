214e2a9c8991843837de138b7af982d1c09b5c79
Виктор Елсуков <beerbis@yandex.ru>
2021-01-22 16:49:36 +0300

    feat: добавить тесты и реализацию метода проверки массива на еденицы и четвёрки

--- task1/testable/src/main/java/Routines.java
+++ task1/testable/src/main/java/Routines.java
@@ -1,9 +1,12 @@
 import java.util.Arrays;
 
 public class Routines {
+    public static final int THE_ONE = 1;
+    public static final int THE_FOUR = 4;
+
     public int[] takeAfter4(int... values) {
         for (int i = values.length; --i >= 0;)
-            if (values[i] == 4) {
+            if (values[i] == THE_FOUR) {
                 i++;
                 return i < values.length
                         ? Arrays.copyOfRange(values, i, values.length)
@@ -11,5 +14,21 @@ public class Routines {
             }
 
         throw new RuntimeException("There are no one four");
+    }
+
+    public static boolean checkIsOneAndFourArray(int... array) {
+        boolean gotOne = false;
+        boolean gotFour = false;
+
+        for (int i = array.length; --i >= 0;)
+            if (array[i] == THE_ONE) {
+                gotOne = true;
+            } else if (array[i] == THE_FOUR) {
+                gotFour = true;
+            } else {
+                return false;
+            }
+
+        return gotOne && gotFour;
     }
 }
