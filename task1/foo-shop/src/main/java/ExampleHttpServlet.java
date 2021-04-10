6cc10bf52fb58e6f6912579ed8007f12665128c3
Виктор Елсуков <beerbis@yandex.ru>
2021-04-11 00:39:31 +0300

    exp: посмотреть что там ходит параметрами

--- task1/foo-shop/src/main/java/ExampleHttpServlet.java
+++ task1/foo-shop/src/main/java/ExampleHttpServlet.java
@@ -3,11 +3,16 @@ import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-@WebServlet(name = "ExampleHttpServlet", urlPatterns = "/http_servlet")
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@WebServlet(name = "ExampleHttpServlet", urlPatterns = "/http_servlet/*")
 public class ExampleHttpServlet extends HttpServlet {
 
     private static Logger logger = LoggerFactory.getLogger(ExampleHttpServlet.class);
@@ -17,6 +22,18 @@ public class ExampleHttpServlet extends HttpServlet {
         logger.info("New GET request");
 
         resp.getWriter().printf("<h1>New GET request</h1>");
+        resp.getWriter().printf("<h1>getContextPath = " + req.getContextPath() + "</h1>");
+        resp.getWriter().printf("<h1>getPathInfo = " + req.getPathInfo() + "</h1>");
+
+        resp.getWriter().printf("<h1>getQueryString = " + req.getQueryString() + "</h1>");
+        Cookie[] cookies = req.getCookies();
+        Stream<Cookie> cookieStream = cookies == null ? Stream.empty() : Arrays.stream(cookies);
+        resp.getWriter().printf("<h1>getCookies = " + cookieStream
+                .map(c -> c.getName() + " = " + c.getValue() + "<br/>")
+                .collect(Collectors.joining(", ")) + "</h1>");
+        resp.addCookie(new Cookie("q" + new Random().nextInt(), "noValue"));
+//        resp.sendRedirect("http://ya.ru");
+        resp.setStatus(500);
     }
 
     @Override
